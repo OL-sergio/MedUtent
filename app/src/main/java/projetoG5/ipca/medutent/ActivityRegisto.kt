8bcccf19f208641e2522239cd9a76bf324d465df
Sérgio <a18074@alunos.ipca.pt>
2020-11-08 16:54:01 +0000

    Registo - Intro

--- app/src/main/java/projetoG5/ipca/medutent/ActivityRegisto.kt
+++ app/src/main/java/projetoG5/ipca/medutent/ActivityRegisto.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
