ffc996933c7bb4e6ea4d1808ad1000999a093f8c
AndreFreitas <64419746+AndFreitas@users.noreply.github.com>
2020-11-20 00:26:35 +0000

    -login

--- app/src/main/java/projetoG5/ipca/medutent/ActivityLogin.kt
+++ app/src/main/java/projetoG5/ipca/medutent/ActivityLogin.kt
@@ -1,4 +1,148 @@
 package projetoG5.ipca.medutent
 
-class ActivityLogin {
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_login.*
+
+
+class ActivityLogin : AppCompatActivity() {
+
+
+    companion object {
+        val TAG = "LoginActivity"
+        val RC_SIGN_IN = 1001
+    }
+
+    private lateinit var auth: FirebaseAuth
+    private var mGoogleSignInClient : GoogleSignInClient? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        buttonLogin.setOnClickListener {
+            auth.signInWithEmailAndPassword(editTextName.text.toString(), editTextPassword.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success")
+                            auth.currentUser
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+
+                            var sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+                            val token = sharedPreferences.getString("firebase_token","")
+                            if ((token?:"").isNotEmpty()){
+                                var auth = Firebase.auth
+                                val currentUser = auth.currentUser
+                                val db = FirebaseFirestore.getInstance()
+
+                                val hashMap = HashMap<String, Any?>()
+                                hashMap["token"] = token
+                                hashMap["email"] = currentUser?.email
+
+                                currentUser?.let {
+                                    db.collection("users")
+                                            .document(currentUser.uid ?:"")
+                                            .set(hashMap)
+                                            .addOnSuccessListener {
+
+                                            }
+                                            .addOnFailureListener {
+
+                                            }
+                                }
+                            }
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.exception)
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+        }
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        buttonLoginGoolge.setOnClickListener {
+            signIn()
+        }
+    }
+
+
+
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+
+
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success")
+                        val user = auth.currentUser
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                        Snackbar.make(buttonLoginGoolge, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+
+                    }
+
+                }
+    }
 }
---
+++ app/src/main/java/projetoG5/ipca/medutent/MainActivity.kt
@@ -0,0 +1,11 @@
+package projetoG5.ipca.medutent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
